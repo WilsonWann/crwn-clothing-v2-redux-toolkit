d60b26694560d54b76d99f654bc2b8ef9a66eb5d
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-03-27 11:13:27 +0800

    hooked with stripe

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from "@stripe/react-stripe-js"
 
 import App from './App';
 import {
-  store,
-  // persistor 
+  store
+  // , persistor 
 } from './store/store';
+import { stripePromise } from './utils/stripe/stripe.utils';
 
 import './index.scss';
 
@@ -18,8 +20,10 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter>
-        <App />
+      <BrowserRouter >
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
       {/* </PersistGate> */}
     </Provider>
